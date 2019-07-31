fe6a2a5051a866b1f8a668ef24556fc30c73865b
Michael <52144224+schardt8237@users.noreply.github.com>
2019-07-30 22:41:52 -0400

    Add files via upload

---
+++ Node.Js/global_objects.js
@@ -0,0 +1,17 @@
+console.log( __filename );
+console.log( __dirname );
+
+function printHello(){
+   console.log( "Hello, World!");
+}
+// Now call above function after 2 seconds
+setTimeout(printHello, 2000);
+
+// Now call above function after 2 seconds
+var t = setTimeout(printHello, 2000);
+
+// Now clear the timer
+clearTimeout(t);
+
+// Now call above function after 2 seconds
+setInterval(printHello, 2000);
